c62604f4d5a24253fdb4d6a199dbab0f7ae1cef6
Guilhem Raux <g.raux@icloud.com>
2023-12-17 01:33:55 +0100

    printing tests output to terminal

--- src/main.rs
+++ src/main.rs
@@ -31,7 +31,7 @@ pub fn exit_qemu(exit_code: QemuExitCode) {
 
 #[cfg(test)]
 fn test_runner(tests: &[&dyn Fn()]) {
-    println!("Running {} tests", tests.len());
+    serial_println!("Running {} tests", tests.len());
     for test in tests {
         test();
     }
@@ -40,9 +40,9 @@ fn test_runner(tests: &[&dyn Fn()]) {
 
 #[test_case]
 fn trivial_assertion() {
-    print!("trivial assertion... ");
+    serial_print!("trivial assertion... ");
     assert_eq!(1, 1);
-    println!("[ok]");
+    serial_println!("[ok]");
 }
 
 #[no_mangle]
