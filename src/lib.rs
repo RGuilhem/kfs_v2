a43f9bb93b5a9987b4249691eb9e74d7222b1dd9
Guilhem Raux <g.raux@icloud.com>
2024-01-16 17:06:43 +0100

    added allocator

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,8 @@
 use bootloader::{BootInfo, entry_point};
 use core::panic::PanicInfo;
 
+extern crate alloc;
+pub mod allocator;
 pub mod memory;
 pub mod gdt;
 pub mod interrupts;
--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,33 @@
 #![test_runner(kfs_v2::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
+extern crate alloc;
+
+use kfs_v2::memory;
+use x86_64::VirtAddr;
 use bootloader::entry_point;
 use bootloader::BootInfo;
 use core::panic::PanicInfo;
 use kfs_v2::println;
+use alloc::boxed::Box;
 
 entry_point!(kernel_main);
 
-pub fn kernel_main(_boot_info: &'static BootInfo) -> ! {
+pub fn kernel_main(boot_info: &'static BootInfo) -> ! {
+    use crate::memory::BootInfoFrameAllocator;
+    use kfs_v2::allocator;
     println!("Start of _start");
     kfs_v2::init();
+
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let mut mapper = unsafe { memory::init(phys_mem_offset) };
+    let mut frame_allocator = unsafe {
+        BootInfoFrameAllocator::init(&boot_info.memory_map)
+    };
+
+    allocator::init_heap(&mut mapper, &mut frame_allocator).expect("Heap initialization failed");
+
+    let test_x = Box::new(41);
 
     #[cfg(test)]
     test_main();
