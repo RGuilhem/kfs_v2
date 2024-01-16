7eb95bd90959ce25e849ded5294bd69fb4dfbf9e
Guilhem Raux <g.raux@icloud.com>
2024-01-16 12:07:46 +0100

    entry_point from bootloader crate in test context

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,8 @@
 #![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
+#[cfg(test)]
+use bootloader::{BootInfo, entry_point};
 use core::panic::PanicInfo;
 
 pub mod gdt;
@@ -51,10 +53,12 @@ pub fn test_panic_handler(info: &PanicInfo) -> ! {
     hlt_loop();
 }
 
+#[cfg(test)]
+entry_point!(test_kernel_main);
+
 /// Entry point for `cargo test`
 #[cfg(test)]
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
+pub fn test_kernel_main(_boot_info: &'static BootInfo) -> ! {
     init();
     test_main();
     hlt_loop();
