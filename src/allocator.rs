88ab10894a9be2e2826142cef583d8daafd59df7
Guilhem Raux <g.raux@icloud.com>
2024-01-18 14:31:42 +0100

    created linke_list allocator

--- src/allocator.rs
+++ src/allocator.rs
@@ -1,15 +1,16 @@
-use crate::allocator::bump::BumpAllocator;
 use x86_64::{
     structures::paging::{
         mapper::MapToError, FrameAllocator, Mapper, Page, PageTableFlags, Size4KiB,
     },
     VirtAddr,
 };
+use crate::allocator::linked_list::LinkedListAllocator;
 
 pub mod bump;
+pub mod linked_list;
 
 #[global_allocator]
-static ALLOCATOR: Locked<BumpAllocator> = Locked::new(BumpAllocator::new());
+static ALLOCATOR: Locked<LinkedListAllocator> = Locked::new(LinkedListAllocator::new());
 
 pub const HEAP_START: usize = 0x_4444_4444_0000;
 pub const HEAP_SIZE: usize = 100 * 1024; // 100 KiB
