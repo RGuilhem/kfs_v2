80aac68f3f848ede276e031b645b7f56167ea784
Guilhem Raux <g.raux@icloud.com>
2024-01-20 15:12:22 +0100

    parsing of debug command into args

--- src/task/console.rs
+++ src/task/console.rs
@@ -3,11 +3,16 @@ use crate::print;
 use crate::println;
 use crate::QemuExitCode;
 use alloc::string::String;
+use alloc::vec::Vec;
 
-pub fn debug_command(command: &String) {
-    if command.len() > 0 {
-        println!("DEBUG: {}", command);
-        match command.as_str() {
+pub fn debug_command(line: &String) {
+    if line.len() > 0 {
+        let split: Vec<&str> = line.split_ascii_whitespace().collect();
+        let command = split[0];
+        let args = &split[1..];
+        println!("DEBUG: command: {}", command);
+        println!("DEBUG: args: {:?}", args);
+        match command {
             "help" => display_help(),
             "exit" => exit_qemu(QemuExitCode::Success),
             &_ => unknown_command(),
